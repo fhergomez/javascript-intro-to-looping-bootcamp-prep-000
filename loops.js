e996f3df21adcf7dd54137ebf38c00e5d6463662
Fernando Gomez <fhergomez37@gmail.com>
2017-09-27 06:52:35 +0000

    automatically backed up by learn

--- loops.js
+++ loops.js
@@ -11,6 +11,13 @@ function forLoop(array){
   return array
 };
 
+function forLoop (array) {
+  for (let i = 0, i < 25, i++) {
+    array.push("I am ${i} strange loop$(i === 1 ? '' : 's')");
+  }
+  return array;
+}
+
 function whileLoop (array) {
 
 }
